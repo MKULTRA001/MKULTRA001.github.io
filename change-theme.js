b62c5f56dad41e95473c00a0f3bb49029f5bbe7d
MKULTRA001 <tim.salazar001@gmail.com>
2019-07-04 17:55:55 -0600

    Update Dark Theme to Light 3.3

--- change-theme.js
+++ change-theme.js
@@ -7,3 +7,19 @@ document.getElementById('change-theme-btn').addEventListener('click', function (
 if (JSON.parse(localStorage.getItem('dark-theme-enabled'))) {
     document.body.classList.add('dark-theme');
 }
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+  
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
